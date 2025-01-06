92f1dbf2ac95023802131d7796912a5cb700428d
dodoyoo <dong15903@daum.net>
2025-01-06 13:36:46 +0900

    feat connect redis to my project

--- src/main/java/com/redis/redis_in_spring/config/RedisConfig.java
+++ src/main/java/com/redis/redis_in_spring/config/RedisConfig.java
@@ -3,6 +3,8 @@ package com.redis.redis_in_spring.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
@@ -12,8 +14,10 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-//    @Bean
-//    public LettuceConnectionFactory redisConnectionFactory() {
-//        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port))
-//    }
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Lettuce라는 라이브러리를 활용해 Redis 연결을 관리하는 객체를 생성하고
+        // Redis 서버에 대한 정보(host, port)를 설정한다.
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
 }
